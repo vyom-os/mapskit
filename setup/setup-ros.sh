d50e32b96b337c71954516950006e3174da3abe9
ParamDeshpande <param@vyomos.org>
2025-11-14 15:31:16 +0530

    refactor(ci): Update cache key and simplify ROS install

--- setup/setup-ros.sh
+++ setup/setup-ros.sh
@@ -1,43 +1,15 @@
 #!/bin/bash
 
-# Update package lists
-sudo apt-get update
-``
-sudo apt update && sudo apt install locales
 
+sudo apt update -y && sudo apt install locales -y 
 sudo locale-gen en_US en_US.UTF-8
-
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-
 export LANG=en_US.UTF-8
 
 sudo apt install software-properties-common -y 
 sudo add-apt-repository universe -y 
 
-sudo apt update && sudo apt install curl -y
-export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
-curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
-sudo dpkg -i /tmp/ros2-apt-source.deb
+sudo apt install ros-humble-ros-base -y
+sudo apt install ros-dev-tools -y 
 
-sudo apt update && sudo apt install -y \
-  python3-flake8-docstrings \
-  python3-pip \
-  python3-pytest-cov \
-  ros-dev-tools
-
-sudo apt install -y \
-    python3-flake8-blind-except \
-    python3-flake8-builtins \
-    python3-flake8-class-newline \
-    python3-flake8-comprehensions \
-    python3-flake8-deprecated \
-    python3-flake8-import-order \
-    python3-flake8-quotes \
-    python3-pytest-repeat \
-    python3-pytest-rerunfailures
-
-
-sudo rosdep init
-rosdep update
-
-
+source /opt/ros/humble/setup.bash
