8f3bcc4dc14241888f5dc5d92ed2c6b98ad6b962
ParamDeshpande <param@vyomos.org>
2025-11-17 15:46:09 +0530

    feat: implement MapsKit class with constructor, destructor, and test method

--- src/mapskit.cpp
+++ src/mapskit.cpp
@@ -1,7 +1,27 @@
-#include "mapskit/cpp_header.hpp"
+#include <pybind11/pybind11.h>
+
+#include "mapskit/mapskit.hpp"
 
 using namespace mapskit;
 
+
+MapsKit::MapsKit() {
+    // Constructor can be empty for now
+}
+
+MapsKit::~MapsKit() {
+    // Destructor can be empty for now
+}
+
 void MapsKit::test_method(void){
-    std::std::cout << "test" << std::endl;
+    std::cout << "hi from test_method" << std::endl;
+}
+
+
+PYBIND11_MODULE(mapskit, m) {
+    m.doc() = "pybind11 mapskit plugin"; // optional module docstring
+
+    py::class_<MapsKit>(m, "MapsKit")
+        .def(py::init<>())
+        .def("test_method", &MapsKit::test_method, "A test method");
 }
