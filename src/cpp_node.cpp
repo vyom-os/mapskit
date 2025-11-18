395bee9fc9dc20d452aac4c79275827f969a9a46
ParamDeshpande <param@vyomos.org>
2025-11-18 18:28:14 +0530

    cpp_node: playing wip

--- src/cpp_node.cpp
+++ src/cpp_node.cpp
@@ -1,11 +1,17 @@
-#include "rclcpp/rclcpp.hpp"
-#include "mapskit/cpp_header.hpp"
+// #include "rclcpp/rclcpp.hpp"
+// #include "mapskit/cpp_header.hpp"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("mapskit_node");
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+    // rclcpp::init(argc, argv);
+    // auto node = std::make_shared<rclcpp::Node>("mapskit_node");
+    // rclcpp::spin(node);
+    // rclcpp::shutdown();
+
+    // std::cout << "Cofigured Prefix Path: " << MY_APP_PREFIX_PATH << std::endl;
+    // You can then use this string in your application logic as needed
+
+
+    return 0;   
 }
