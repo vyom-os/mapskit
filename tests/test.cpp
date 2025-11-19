6c0b3a11aca70ebc2a01106dce48578bbb681852
ParamDeshpande <param@vyomos.org>
2025-11-19 18:30:56 +0530

    tests: added fkyaml and testing

---
+++ tests/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// #include "mapskit/mapskit.hpp"
+
+int main(int argc, char **argv)
+{
+    // rclcpp::init(argc, argv);
+    // auto node = std::make_shared<rclcpp::Node>("mapskit_node");
+    // rclcpp::spin(node);
+    // rclcpp::shutdown();
+
+    // std::cout << "Cofigured Prefix Path: " << MAPSKIT_CONFIG_PATH << std::endl;
+    // std::cout << "Cofigured Launch Path: " << MAPSKIT_LAUNCH_PATH << std::endl;
+
+    return 0;   
+}
---
+++ tests/tutorial.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <fkYAML/node.hpp>
+
+int main() {
+    // open a YAML file. Other streams or strings are also usable as an input.
+    std::ifstream ifs("/vyomos/playground/mapskit/tests/example.yaml");
+
+    // deserialize the loaded file contents.
+    fkyaml::node root = fkyaml::node::deserialize(ifs);
+
+    // print the deserialized YAML nodes by serializing them back.
+    std::cout << root << std::endl;
+    return 0;
+}
