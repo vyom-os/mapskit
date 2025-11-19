021349a009af49754195cf1c6e30dbe70ff464c9
ParamDeshpande <param@vyomos.org>
2025-11-19 18:33:14 +0530

    main.cpp : helloworld basic init

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// #include "mapskit/mapskit.hpp"
+// #include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char **argv)
+{
+    // rclcpp::init(argc, argv);
+    // auto node = std::make_shared<rclcpp::Node>("mapskit_node");
+    // rclcpp::spin(node);
+    // rclcpp::shutdown();
+
+    // std::cout << "Cofigured Prefix Path: " << MAPSKIT_CONFIG_PATH << std::endl;
+    // std::cout << "Cofigured Launch Path: " << MAPSKIT_LAUNCH_PATH << std::endl;
+
+    return 0;   
+}
