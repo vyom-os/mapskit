a0f4a8364344b6121704c6f472f4a1f143d2337d
ParamDeshpande <param@vyomos.org>
2025-11-20 18:43:28 +0530

    main.cpp: create a mpaskit obj & run default

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <sys/wait.h>
 
-// #include "mapskit/mapskit.hpp"
-// #include "rclcpp/rclcpp.hpp"
+#include "mapskit/mapskit.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char **argv)
 {
+
+    std::cout << "Cofigured Prefix Path: " << MAPSKIT_CONFIG_PATH << std::endl;
+    std::cout << "Cofigured Launch Path: " << MAPSKIT_LAUNCH_PATH << std::endl;
+
+    mapskit::MapsKit mk;
+    int pid = mk.run_server();
+    std::cout << "pid is" << pid << " \n";
+    waitpid(pid, nullptr, 0);
     // rclcpp::init(argc, argv);
-    // auto node = std::make_shared<rclcpp::Node>("mapskit_node");
+    // auto node = std::make_shared<rclcpp::Node>("mapskit_core_node");
     // rclcpp::spin(node);
     // rclcpp::shutdown();
-
-    // std::cout << "Cofigured Prefix Path: " << MAPSKIT_CONFIG_PATH << std::endl;
-    // std::cout << "Cofigured Launch Path: " << MAPSKIT_LAUNCH_PATH << std::endl;
 
     return 0;   
 }
