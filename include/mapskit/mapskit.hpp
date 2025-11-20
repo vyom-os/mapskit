4f8e7aae773bec7c618f2af3a9a798298900ba89
ParamDeshpande <param@vyomos.org>
2025-11-20 18:42:32 +0530

    mapskit.hpp : init defs pub / priv for class definitions

--- include/mapskit/mapskit.hpp
+++ include/mapskit/mapskit.hpp
@@ -1,9 +1,14 @@
 #ifndef MAPSKIT_HPP
 #define MAPSKIT_HPP
 
+/* pybind11/pybind11.h needs to be the 1st include file */
 #include <pybind11/pybind11.h>
+
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+
+// #include <fkYAML/node.hpp>
 
 namespace py = pybind11;
 
@@ -13,10 +18,13 @@ class MapsKit {
 public:
     // Constructor
     MapsKit();
+    MapsKit(std::string conf_yml_pth);
     
     // Destructor
     ~MapsKit();
     
+    
+
     // Copy constructor
     MapsKit(const MapsKit& other);
     
@@ -32,10 +40,15 @@ public:
     // Public member functions
     void initialize();
     void test_method(void);
-    
+    int run_server(void);
+    void shutdown_server(void);    
+
 private:
     // Private member variables
     // Private member functions
+    std::string config_path_{'\0'};
+    const std::string launch_path_ = MAPSKIT_LAUNCH_PATH;
+    volatile int server_pid_{0};
 };
 
 } // namespace mapskit
