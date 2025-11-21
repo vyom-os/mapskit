6cea28b4b6cf7399caac3d54854a259baccd6ad8
ParamDeshpande <param@vyomos.org>
2025-11-21 11:30:19 +0530

    main.cpp: add the config file as an arg

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sys/wait.h>
+#include <memory>
 
 #include "mapskit/mapskit.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -10,8 +11,16 @@ int main(int argc, char **argv)
     std::cout << "Cofigured Prefix Path: " << MAPSKIT_CONFIG_PATH << std::endl;
     std::cout << "Cofigured Launch Path: " << MAPSKIT_LAUNCH_PATH << std::endl;
 
-    mapskit::MapsKit mk;
-    int pid = mk.run_server();
+    std::unique_ptr<mapskit::MapsKit> mk;
+    if (argc > 1) {
+        std::cout << "argv[1]: " << argv[1] << std::endl;
+        
+        mk = std::make_unique<mapskit::MapsKit>(argv[1]);
+    } else {
+        mk = std::make_unique<mapskit::MapsKit>();
+    }
+
+    int pid = mk->run_server();
     std::cout << "pid is" << pid << " \n";
     waitpid(pid, nullptr, 0);
     // rclcpp::init(argc, argv);
